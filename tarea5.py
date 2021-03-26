5f3c3c0047574260189886393a2e9880ea43f3b3
BrianCruz15 <briancruz15@aragon.unam.mx>
2021-03-25 22:04:35 -0600

    Tarea número 5

---
+++ tarea5.py
@@ -0,0 +1,2 @@
+Organización: Se encarga de brincar un servicio orientado a la satisfacción de las necesidades del usuario. La organización en un centro de computo ayuda a segmentar las diferentes áreas o departamentos de una empresa, identificando puestos de trabajado, designando las actividades y funciones de cada uno de los integrantes, hasta la organización del espacio de trabajo. 
+  
